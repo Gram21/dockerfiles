06bf1ebea5b077df28b51c3078e7089b4ffebefd
Jan Keim <jan.keim@kit.edu>
2022-12-02 14:40:03 +0100

    Update latex-remote

--- latex-remote/run.sh
+++ latex-remote/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -d -p 0.0.0.0:2222:22 --name latex --restart unless-stopped latex
+podman run -d -p 0.0.0.0:2222:22 --name latex --restart always -v /home/jan/latex/repos:/root/repos latex
